f888eecb0e64a8760b187fb1329c78d79a478f8c
IseLein <iseoluwagbotemidaniel@gmail.com>
2024-12-21 07:33:25 -0500

    day21

--- day21.cpp
+++ day21.cpp
@@ -7,7 +7,9 @@
 #include <utility>
 #include <vector>
 
-#define DEBUG 1
+#define ll long long
+#define DEBUG false
+#define IFD(x) if (DEBUG) { x; }
 
 using namespace std;
 
@@ -24,10 +26,9 @@ const vector<vector<char>> DKEYPAD = {
 
 map<tuple<int, int, int, pair<int, int>>, string> BEST_DIRECTIONS_CACHE = {};
 
-map<tuple<string, int>, long> SEQUENCE_LENGTH_CACHE = {};
+map<tuple<string, int>, ll> SEQUENCE_LENGTH_CACHE = {};
 
-string getDirections(vector<vector<char>> keypad, string key, int level);
-long getDirections2(vector<vector<char>> keypad, string key, int level);
+ll getDirections2(vector<vector<char>> keypad, string key, int level);
 
 void printKeypad(vector<vector<char>> keypad) {
     for (auto& row : keypad) {
@@ -38,7 +39,6 @@ void printKeypad(vector<vector<char>> keypad) {
     }
 }
 
-
 int main() {
     ifstream fin("input/day21");
     vector<string> keys;
@@ -47,36 +47,16 @@ int main() {
         keys.push_back(line);
     }
 
-    /*for (auto &key: keys) {*/
-    /*    cout << key << "\n";*/
-    /*    int level = 2;*/
-    /*    string dir = getDirections(KEYPAD, key, level);*/
-    /*    cout << level << ": " << dir << "\n";*/
-    /*    level--;*/
-    /*    for (int i = level; i >= 0; i--) {*/
-    /*        dir = getDirections(DKEYPAD, dir, i);*/
-    /*        cout << i << ": " << dir << "\n";*/
-    /*    }*/
-    /**/
-    /*    long length = dir.size();*/
-    /*    long num = stol(key.substr(0, 3));*/
-    /*    long score = num * length;*/
-    /*    total += score;*/
-    /*    cout << key << ": " << num << " * " << length << " = " << score << "\n";*/
-    /*}*/
-    /*cout  << "Total: " << total << "\n";*/
-
-    long total = 0;
+    ll total = 0;
     for (int i = 0; i < (int)keys.size(); i++) {
         string key = keys[i];
-        int level = 3;
-        long length = getDirections2(KEYPAD, key, level);
-        cout << key << ": " << length << "\n";
+        int level = 26;
+        ll length = getDirections2(KEYPAD, key, level);
 
-        long num = stol(key.substr(0, 3));
-        long score = num * length;
+        ll num = stoll(key.substr(0, 3));
+        ll score = num * length;
         total += score;
-        // cout << key << ": " << num << " * " << length << " = " << score << "\n";
+        cout << key << ": " << num << " * " << length << " = " << score << "\n";
     }
     cout << "Total: " << total << "\n";
 }
@@ -114,35 +94,6 @@ bool _isValidDir(vector<vector<char>> keypad, string dir, pair<int, int> pos) {
     return true;
 }
 
-string _getDir(string key) {
-    pair<int, int> start;
-    _findChar(DKEYPAD, 'A', start);
-
-    key += "A";
-
-    string directions = "";
-    for (auto &c: key) {
-        pair<int, int> next;
-        _findChar(DKEYPAD, c, next);
-
-        int yDiff = next.first - start.first;
-        if (yDiff > 0) {
-            directions += string(yDiff, 'v');
-        } else if (yDiff < 0) {
-            directions += string(-yDiff, '^');
-        }
-        int xDiff = next.second - start.second;
-        if (xDiff > 0) {
-            directions += string(xDiff, '>');
-        } else if (xDiff < 0) {
-            directions += string(-xDiff, '<');
-        }
-        directions += 'A';
-        start = next;
-    }
-    return directions;
-}
-
 vector<string> _findArrangements(int numX, int numY, char charX, char charY) {
     vector<string> arr;
     if (numX == 0) {
@@ -169,126 +120,19 @@ vector<string> _findArrangements(int numX, int numY, char charX, char charY) {
     return arr;
 }
 
-string _bestDirection(int xDiff, int yDiff, pair<int, int> pos, vector<vector<char>> keypad, int level) {
-    tuple<int, int, int, pair<int, int>> key = make_tuple(xDiff, yDiff, level, pos);
-    if (BEST_DIRECTIONS_CACHE.count(key) != 0) {
-        return BEST_DIRECTIONS_CACHE.at(key);
-    }
-    if (xDiff == 0 && yDiff == 0) {
-        return "";
-    }
-    // unimportant defaults as they'll be assigned below
-    int numX = 0;
-    int numY = 0;
-    char charX = 'A';
-    char charY = 'A';
-    if (yDiff > 0) {
-        numY = yDiff;
-        charY = 'v';
-    } else if (yDiff < 0) {
-        numY = -yDiff;
-        charY = '^';
-    }
-    if (xDiff > 0) {
-        numX = xDiff;
-        charX = '>';
-    } else if (xDiff < 0) {
-        numX = -xDiff;
-        charX = '<';
-    }
-    vector<string> arrangements = _findArrangements(numX, numY, charX, charY);
-    map<int, vector<string>> bestDir;
-    map<string, string> nextDir2Arr;
-    int bestScore = INT_MAX;
-    for (auto &arrangement: arrangements) {
-        if (!_isValidDir(keypad, arrangement, pos)) {
-            continue;
-        }
-        string nextLevelDir = _getDir(arrangement);
-        int score = nextLevelDir.size();
-        if (bestDir.count(score) == 0) {
-            bestDir[score] = vector<string>();
-        }
-        bestDir[score].push_back(nextLevelDir);
-        if (score < bestScore) {
-            bestScore = score;
-        }
-        nextDir2Arr[nextLevelDir] = arrangement;
-        cout << "arrangement: " << arrangement << ": " << nextLevelDir << "\n";
-    }
-
-
-    // best next levels
-    vector<string> bestDirs = bestDir[bestScore];
-    if (bestDirs.size() == 1) {
-        string dir = nextDir2Arr[bestDirs[0]];
-        BEST_DIRECTIONS_CACHE.insert(make_pair(key, dir));
-        return dir;
-        return nextDir2Arr[bestDirs[0]];
-    }
-
-    level--;
-    while ((level > 0) && (bestDirs.size() > 1)) {
-        map<int, vector<string>> bestDir2;
-        map<string, string> nextDir2Arr2;
-        int bestScore = INT_MAX;
-        for (auto &dir: bestDirs) {
-            string nextLevelDir = _getDir(dir);
-            int score = nextLevelDir.size();
-            if (bestDir2.count(score) == 0) {
-                bestDir2[score] = vector<string>();
-            }
-            bestDir2[score].push_back(nextLevelDir);
-            if (score < bestScore) {
-                bestScore = score;
-            }
-            nextDir2Arr2[nextLevelDir] = nextDir2Arr[dir];
-            cout << "level " << level << ": " << nextDir2Arr[dir] << " -> " << dir << " -> " << nextLevelDir << "\n";
-        }
-        bestDirs = bestDir2[bestScore];
-        nextDir2Arr = nextDir2Arr2;
-        level--;
-    }
-
-    string dir = nextDir2Arr[bestDirs[0]];
-    BEST_DIRECTIONS_CACHE.insert(make_pair(key, dir));
-    return dir;
-}
-
-string getDirections(vector<vector<char>> keypad, string key, int level) {
-    pair<int, int> start;
-    _findChar(keypad, 'A', start);
-    // cout << "start at " << start.first << ", " << start.second << "\n";
-
-    string directions = "";
-    for (auto &c: key) {
-        pair<int, int> next;
-        _findChar(keypad, c, next);
-
-        // cout << "going to " << c << " at " << next.first << ", " << next.second << "\n";
-
-        int yDiff = next.first - start.first;
-        int xDiff = next.second - start.second;
-        directions += _bestDirection(xDiff, yDiff, start, keypad, level);
-        directions += 'A';
-        start = next;
-    }
-    return directions;
-}
-
-long sequenceLength(string s, int level) {
-    cout << "level: " << level << ": " << s << "\n";
+ll sequenceLength(string s, int level) {
+    IFD(cout << "level: " << level << ": " << s << "\n");
     if (level == 0) {
         return s.size();
     }
     if (SEQUENCE_LENGTH_CACHE.count(make_pair(s, level)) != 0) {
-        cout << "cache hit\n";
+        IFD(cout << "cache hit\n");
         return SEQUENCE_LENGTH_CACHE.at(make_pair(s, level));
     }
 
     pair<int, int> start;
     _findChar(DKEYPAD, 'A', start);
-    long total = 0;
+    ll total = 0;
     for (auto &c: s) {
         pair<int, int> next;
         _findChar(DKEYPAD, c, next);
@@ -312,28 +156,29 @@ long sequenceLength(string s, int level) {
             charX = '<';
         }
         vector<string> arrangements = _findArrangements(numX, numY, charX, charY);
-        vector<int> lengths;
+        vector<ll> lengths;
         for (auto &arrangement: arrangements) {
             if (!_isValidDir(DKEYPAD, arrangement, start)) {
                 continue;
             }
             arrangement += 'A';
-            cout << "arrangement: " << arrangement << "\n";
+            IFD(cout << "arrangement: " << arrangement << "\n");
             lengths.push_back(sequenceLength(arrangement, level - 1));
         }
-        cout << "\n";
+        IFD(cout << "\n");
         sort(lengths.begin(), lengths.end());
         total += lengths[0];
         start = next;
     }
+    SEQUENCE_LENGTH_CACHE.insert(make_pair(make_tuple(s, level), total));
     return total;
 }
 
-long getDirections2(vector<vector<char>> keypad, string key, int level) {
+ll getDirections2(vector<vector<char>> keypad, string key, int level) {
     pair<int, int> start;
     _findChar(keypad, 'A', start);
 
-    int total = 0;
+    ll total = 0;
     for (auto &c: key) {
         pair<int, int> next;
         _findChar(keypad, c, next);
@@ -358,16 +203,16 @@ long getDirections2(vector<vector<char>> keypad, string key, int level) {
             charX = '<';
         }
         vector<string> arrangements = _findArrangements(numX, numY, charX, charY);
-        vector<int> lengths;
+        vector<ll> lengths;
         for (auto &arrangement: arrangements) {
             if (!_isValidDir(keypad, arrangement, start)) {
                 continue;
             }
             arrangement += 'A';
-            cout << "arrangement: " << arrangement << "\n";
+            IFD(cout << "arrangement: " << arrangement << "\n");
             lengths.push_back(sequenceLength(arrangement, level - 1));
         }
-        cout << "\n";
+        IFD(cout << "\n");
         sort(lengths.begin(), lengths.end());
         total += lengths[0];
         start = next;
